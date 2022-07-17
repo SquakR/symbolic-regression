7b043ccaa000e64c017c5df95cc62c37d2592492
squak <squakrazv@gmail.com>
2022-07-18 01:08:00 +0300

    feat: add expression tree base types

--- rust-lib/src/expression_tree.rs
+++ rust-lib/src/expression_tree.rs
@@ -1,1 +1,65 @@
-pub struct Node {}
+pub enum UnaryOperationKind {
+    Inversion,
+    Factorial,
+    Exp,
+    Ln,
+    Sin,
+    Arcsin,
+    Cos,
+    Arccos,
+    Tan,
+    Arctan,
+    Cot,
+    Arccot,
+    Sinh,
+    Arsinh,
+    Cosh,
+    Arcosh,
+    Tanh,
+    Artanh,
+    Coth,
+    Arcoth,
+}
+
+pub struct UnaryOperation<'a> {
+    pub kind: UnaryOperationKind,
+    pub argument: Box<Node<'a>>,
+}
+
+pub enum BinaryOperationKind {
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+    Degree,
+    Logarithm,
+}
+
+pub struct BinaryOperation<'a> {
+    pub kind: BinaryOperationKind,
+    pub first_argument: Box<Node<'a>>,
+    pub second_argument: Box<Node<'a>>,
+}
+
+pub struct Variable {
+    pub name: String,
+    pub value: Option<f64>,
+}
+
+pub enum Value<'a> {
+    Variable(&'a Variable),
+    Value(f64),
+}
+
+pub enum Node<'a> {
+    UnaryOperation(UnaryOperation<'a>),
+    BinaryOperation(BinaryOperation<'a>),
+    Value(Value<'a>),
+}
+
+pub struct Tree<'a> {
+    root: Node<'a>,
+    variables: Vec<Variable>,
+}
+
+impl<'a> Tree<'a> {}
--- rust-lib/src/lib.rs
+++ rust-lib/src/lib.rs
@@ -3,11 +3,10 @@ mod expression_tree;
 #[cfg(test)]
 mod expression_tree_tests {
     use super::*;
-    use expression_tree::Node;
+    use expression_tree::Tree;
 
     #[test]
     fn it_works() {
-        let node = Node {};
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
