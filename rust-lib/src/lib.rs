0217c1135b54671bdc11e21603ebcbdefecd969b
squak <squakrazv@gmail.com>
2022-07-17 23:57:46 +0300

    feat: add expression_tree module

--- rust-lib/src/lib.rs
+++ rust-lib/src/lib.rs
@@ -1,7 +1,13 @@
+mod expression_tree;
+
 #[cfg(test)]
-mod tests {
+mod expression_tree_tests {
+    use super::*;
+    use expression_tree::Node;
+
     #[test]
     fn it_works() {
+        let node = Node {};
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
