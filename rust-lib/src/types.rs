0e0a51aa742595c4be99ce6dd38a115740065441
squak <squakrazv@gmail.com>
2022-08-08 05:20:30 +0300

    feat: add FunctionConverter struct

--- rust-lib/src/types.rs
+++ rust-lib/src/types.rs
@@ -113,6 +113,31 @@ pub enum Associativity {
     Right,
 }
 
+pub struct FunctionConverter<'a> {
+    pub from: &'a Function,
+    pub to: &'a Function,
+    pub is_conversion_possible_fn: fn(&'a [f64]) -> bool,
+    pub convert_fn: fn(&'a [f64]) -> Vec<f64>,
+}
+
+impl<'a> FunctionConverter<'a> {
+    fn is_conversion_possible(&self, function: &'a Function, arguments: &'a [f64]) -> bool {
+        function == self.from && (self.is_conversion_possible_fn)(arguments)
+    }
+    fn convert(&self, arguments: &'a [f64]) -> ConvertOutputData<'a> {
+        ConvertOutputData {
+            function: self.to,
+            arguments: (self.convert_fn)(arguments),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct ConvertOutputData<'a> {
+    pub function: &'a Function,
+    pub arguments: Vec<f64>,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -164,10 +189,11 @@ mod tests {
 
     mod function_tests {
         use super::*;
+        use std::f64::consts::E;
 
         #[test]
         fn test_debug() {
-            let test_function = create_test_function();
+            let test_function = create_sin_function();
             assert_eq!(
                 "Function { name: \"sin\", arguments_number: 1, complexity: 1, io_only: false }",
                 format!("{:?}", test_function)
@@ -176,14 +202,14 @@ mod tests {
 
         #[test]
         fn test_display() {
-            let test_function = create_test_function();
+            let test_function = create_sin_function();
             assert_eq!("sin", format!("{}", test_function));
         }
 
         #[test]
         fn test_eq() {
-            let test_function1 = create_test_function();
-            let mut test_function2 = create_test_function();
+            let test_function1 = create_sin_function();
+            let mut test_function2 = create_sin_function();
             assert!(test_function1 == test_function2);
             test_function2.name = String::from("cos");
             assert!(test_function1 != test_function2);
@@ -191,24 +217,80 @@ mod tests {
 
         #[test]
         fn test_compute() {
-            let test_function = create_test_function();
+            let test_function = create_sin_function();
             assert_eq!(2.0_f64.sin(), test_function.compute(&[2.0]));
         }
 
         #[test]
         #[should_panic(expected = "The function `sin` expected 1 arguments but received 2.")]
         fn test_compute_panic() {
-            let test_function = create_test_function();
+            let test_function = create_sin_function();
             assert_eq!(2.0_f64.sin(), test_function.compute(&[1.0, 2.0]));
         }
 
-        fn create_test_function() -> Function {
+        #[test]
+        fn test_is_conversion_possible() {
+            let log_function = create_log_function();
+            let ln_function = create_ln_function();
+            let converter = FunctionConverter {
+                from: &log_function,
+                to: &ln_function,
+                is_conversion_possible_fn: |arguments| (arguments[0] - E).abs() <= 0.001,
+                convert_fn: |arguments| vec![arguments[1]],
+            };
+            let possible_arguments = vec![E + 0.0001, 10.0];
+            let not_possible_arguments = vec![E + 0.01, 10.0];
+            assert!(converter.is_conversion_possible(&log_function, &possible_arguments));
+            assert!(!converter.is_conversion_possible(&ln_function, &not_possible_arguments));
+        }
+
+        #[test]
+        fn test_convert() {
+            let log_function = create_log_function();
+            let ln_function = create_ln_function();
+            let converter = FunctionConverter {
+                from: &log_function,
+                to: &ln_function,
+                is_conversion_possible_fn: |arguments| (arguments[0] - E).abs() <= 0.001,
+                convert_fn: |arguments| vec![arguments[1]],
+            };
+            let arguments = vec![E + 0.0001, 10.0];
+            assert_eq!(
+                ConvertOutputData {
+                    function: &ln_function,
+                    arguments: vec![10.0]
+                },
+                converter.convert(&arguments)
+            );
+        }
+
+        fn create_sin_function() -> Function {
             Function {
                 name: String::from("sin"),
                 arguments_number: 1,
                 complexity: 1,
                 io_only: false,
                 compute_fn: |arguments| arguments[0].sin(),
+            }
+        }
+
+        fn create_log_function() -> Function {
+            Function {
+                name: String::from("log"),
+                arguments_number: 2,
+                complexity: 1,
+                io_only: false,
+                compute_fn: |arguments| arguments[0].log(arguments[1]),
+            }
+        }
+
+        fn create_ln_function() -> Function {
+            Function {
+                name: String::from("ln"),
+                arguments_number: 1,
+                complexity: 1,
+                io_only: true,
+                compute_fn: |arguments| arguments[0].ln(),
             }
         }
     }
